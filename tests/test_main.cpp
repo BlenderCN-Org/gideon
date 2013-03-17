3b6207a48f6b8fe511db854860963a541cfba589
Curtis Andrus <curtis@the-tardis.(none)>
2013-03-17 16:23:23 -0700

    Modified build system to support LLVM. The file test_main.cpp also compiles a demo module for testing.

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,155 +1,166 @@
-#include "vm/vm.hpp"
-#include "vm/instruction.hpp"
-#include "vm/standard.hpp"
-#include "scene/scene.hpp"
+#include "llvm/ExecutionEngine/ExecutionEngine.h"
+#include "llvm/ExecutionEngine/JIT.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/PassManager.h"
+#include "llvm/Analysis/Passes.h"
+#include "llvm/DataLayout.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/Attributes.h"
+
+#include "math/vector.hpp"
+
+#include "compiler/ast/ast.hpp"
+#include "compiler/types.hpp"
+#include "compiler/symboltable.hpp"
+
+#include "rtlparser.hpp"
+#include "rtlscanner.hpp"
 
 #include <iostream>
 
 using namespace std;
 using namespace raytrace;
+using namespace llvm;
 
-void test_range(vm &svm, instruction_set &is, int start, int end) {
-  svm.constants.get<int>(0) = 1;
-  svm.constants.get<int>(1) = start;
-  svm.constants.get<int>(2) = end;
+struct rtl_parse_data {
+  var_symbol_table *variables;
+  func_symbol_table *functions;
+  control_state *control;
+  vector<ast::global_declaration_ptr> *ast;
+};
 
-  vector<instruction> prog;
-  assemble(is, prog,
-	   vector<string>{
-	     "mov_i_iC 0 1",
-	       "sub_i_iC 1 0 2",
-	       "jump_zero 1 5",
-	       "add_i_iC 0 0 0",
-	       "jump 1"
-	       });
-  
-  //execute the program
-  while (svm.pc < prog.size()) {
-    cout << "PC: " << svm.pc << endl;
-    svm.execute(prog[svm.pc]);
-  }
+int yyparse(yyscan_t scanner, rtl_parse_data *rtl_data);
 
-  cout << "Loop Counter: " << svm.get<int>(0) << endl;
+void compile_string(const std::string &source,
+		    var_symbol_table &vars, func_symbol_table &funcs,
+		    control_state &control,
+		    vector<ast::global_declaration_ptr> &ast) {
+  yyscan_t scanner;
+
+  if (yylex_init(&scanner)) { throw runtime_error("Could not initialize yylex"); }
+  YY_BUFFER_STATE state = yy_scan_string(source.c_str(), scanner);
+
+  rtl_parse_data data = { &vars, &funcs, &control, &ast };
+  if (yyparse(scanner, &data)) { throw runtime_error("Parser error"); }  
+
+  yy_delete_buffer(state, scanner);
+  yylex_destroy(scanner);
 }
 
-void sample_pixels(vm &svm, instruction_set &is, int x_pixels, int y_pixels) {
-  svm.constants.get<float>(0) = x_pixels;
-  svm.constants.get<float>(1) = y_pixels;
-  svm.constants.get<float>(2) = 1.0f;
-  svm.constants.get<float>(3) = 0.0f;
 
-  svm.constants.get<int>(0) = 0;
-  svm.constants.get<int>(1) = x_pixels;
-  svm.constants.get<int>(2) = y_pixels;
-  svm.constants.get<int>(3) = 1;
-
-  vector<instruction> prog;
-  assemble(is, prog,
-	   {
-	     //compute pixel dx/dy
-	     //dx = 1.0 / x_pixels  |  dy = 1.0 / y_pixels
-	     "div_fC_fC 0 2 0", //r0 = dx
-	       "div_fC_fC 1 2 1", //r1 = dy
-	       
-	       //setup x loop counter
-	       "mov_f_fC 2 3", //x = 0.0
-	       "mov_i_iC 0 0", //i = 0
-
-	       //setup y loop
-	       "mov_f_fC 3 3", //y = 0.0
-	       "mov_i_iC 1 0", //j = 0
-
-	       //increment y
-	       "add_f_f 3 1 3", //y += dy
-	       "add_i_iC 1 1 3", //j += 1
-	       
-	       //y loop
-	       "sub_i_iC 2 1 2", //tmp = j - y_pixels
-	       "jump_nz 2 6", //continue if j != y_pixels
-	       
-	       //increment x
-	       "add_f_f 2 0 2", //x += dx
-	       "add_i_iC 0 0 3", //i += 1
-
-	       //x loop
-	       "sub_i_iC 2 0 1", //tmp = i - x_pixels
-	       "jump_nz 2 4" //continue if i != x_pixels
-	       });
-
-  while (svm.pc < prog.size()) {
-    svm.execute(prog[svm.pc]);
-  }
-
-  cout << "Dx: " << svm.get<float>(0) << " | Dy: " << svm.get<float>(1) << endl;
-  cout << "Pixels: [" << svm.get<int>(0) << ", " << svm.get<int>(1) << "]" << endl;
-}
-
-void func_call(vm &svm, instruction_set &is) {
-  svm.constants.get<float>(0) = 13.0f;
-  svm.constants.get<float>(1) = 2.3f;
-  
-  svm.constants.get<int>(0) = 1;
-  
-  vector<instruction> prog;
-  assemble(is, prog,
-	   {
-	     "jump 3", //jump to start
-
-	       //f(x) = x + 13
-	       "add_f_fC -1 0 0", 
-	       "jump_R 1", //return
-
-	       //start
-	       "mov_f_fC 128 1", //set the arguments
-	       "mov_i_iC 128 0", //set the function location
-
-	       //function call
-	       "stack_push",
-	       "save_pc_offset 1 2",
-	       "jump_R 0",
-	       "stack_pop",
-
-	       "mov_f_f 0 127"
-	       });
-
-  while (svm.pc < prog.size()) { 
-    cout << "PC: " << svm.pc << endl;
-    svm.execute(prog[svm.pc]);
-  }
-
-  cout << "Result: " << svm.get<float>(0) << endl;
-}
 
 int main(int argc, char **argv) {
-  vm svm(NULL, NULL);
-  instruction_set is = create_standard_instruction_set();
-
-  attribute_type t = get_attribute_type<int3>();
-  attribute_type ref = {attribute_type::FLOAT, attribute_type::SCALAR};
-  cout << "T: " << t.base_type << " | " << ref.base_type << endl;
-  cout << "Agg: " << t.aggregate_type << " | " << ref.aggregate_type << endl;
+  InitializeNativeTarget();
+  LLVMContext &ctx = getGlobalContext();
+  Module *module = new Module("shader_test", ctx);
+  IRBuilder<> builder(ctx);
   
-  /*instruction i0 = is.make("add_f3C_f3C", 0, 0, 1);
-  instruction i1 = is.make("mul_fC_f3", 0, 1, 0);
-
-  cout << "Float3 Capacity: " << svm.constants.capacity<float3>() << endl;
-  cout << "Ray Capacity: " << svm.registers.capacity<ray>() << endl;
-
-  svm.constants.get<float3>(0) = float3{0.0f, 0.0f, 4.0f};
-  svm.constants.get<float3>(1) = float3{0.0f, 2.0f, 3.0f};
-  svm.constants.get<float>(1) = 0.5f;
-
-  cout << "Executing Instruction " << i0.id << "... " << endl;
-  svm.execute(i0);
-  svm.execute(i1);
-  cout << "Done." << endl;
-
-  float3 &v = svm.registers.get<float3>(0);
-  cout << "F3_0: {" << v.x << ", " << v.y << ", " << v.z << "}" << endl;*/
+  string error_str;
+  ExecutionEngine *engine = EngineBuilder(module).setErrorStr(&error_str).create();
   
-  //test_range(svm, is, 0, 4);
-  //sample_pixels(svm, is, 3, 3);
-  func_call(svm, is);
+  FunctionPassManager fpm(module);
+
+  // Set up the optimizer pipeline.  Start with registering info about how the
+  // target lays out data structures.
+  fpm.add(new DataLayout(*engine->getDataLayout()));
   
+  fpm.add(createPromoteMemoryToRegisterPass());
+
+
+  // Provide basic AliasAnalysis support for GVN.
+  fpm.add(createBasicAliasAnalysisPass());
+  // Do simple "peephole" optimizations and bit-twiddling optzns.
+  fpm.add(createInstructionCombiningPass());
+  // Reassociate expressions.
+  fpm.add(createReassociatePass());
+  // Eliminate Common SubExpressions.
+  fpm.add(createGVNPass());
+  // Simplify the control flow graph (deleting unreachable blocks, etc).
+  fpm.add(createCFGSimplificationPass());
+
+  fpm.doInitialization();
+  
+  var_symbol_table symbols;
+  func_symbol_table functions;
+  control_state control;
+  vector<ast::global_declaration_ptr> ast;
+  
+  compile_string("extern float sin(float x) : sinf;\n"
+		 "void incr(output float x) { x = x + 1.0; }\n"
+		 "void incr_int(output int i) { i = i + 1; }\n"
+		 "int test_ints(int a, int m) {\n"
+		 "  int v = 0;\n"
+		 "  if (a < m) {\n"
+		 "  for (int i = a; i < m; incr_int(i)) {\n"
+		 "    v = v + 1;\n"
+		 "    break;\n"
+		 "  }\n"
+		 "  }\n"
+		 "  return v;\n"
+		 "}\n"
+		 "vec4 add_vectors(vec4 in) { return in + vec4(1.0, 1.0, 1.0, 1.0); }\n"
+		 "float foo(float a, output float b, output vec4 in_x, output vec4 out_x) {\n"
+		 "  incr(a);\n"
+		 "  b = b + a;\n"
+		 "  out_x = add_vectors(in_x);\n"
+		 "  return a;\n"
+		 "}\n"
+		 "float test_sin(float x) { return sin(x); }\n",
+		 symbols, functions, control, ast);
+  
+  for (auto gd_it = ast.begin(); gd_it != ast.end(); gd_it++) (*gd_it)->codegen(module, builder);
+  
+  bool do_optimize = true;
+  if (do_optimize) {
+    Function *f = functions.get("foo").func;
+    fpm.run(*f);
+    
+    f = functions.get("incr").func;
+    fpm.run(*f);
+    
+    f = functions.get("add_vectors").func;
+    fpm.run(*f);
+
+    f = functions.get("test_ints").func;
+    fpm.run(*f);
+  }
+    
+  verifyModule(*module);
+  //module->dump();
+  
+  void *fptr;
+  fptr = engine->getPointerToFunction(functions.get("foo").func);
+  /*float (*foo)(float, float, bool, float) = (float (*)(float, float, bool, float))(fptr);
+  
+  float rt;
+  float x = 2.2f, z = -0.56f;
+  cout << "foo(x = " << x << ", z = " << z << ", false) = " << foo(x, z, false, rt) << " | Expected: " << 3.2 + x + x + z + 2.0 << endl;
+  cout << "Pointer Size: " << sizeof(ast::expression_ptr) << endl;*/
+  
+  float (*foo)(float, float*, float4*, float4 *) = (float (*)(float, float*, float4*, float4*))(fptr);
+
+  float4 x{1.0f, 2.0f, 3.0f, 4.0f};
+  float4 y;
+
+  float a = 0.0;
+  float b = 5.0;
+  cout << "foo(a = " << a << ", &b, &x, &y) = " << foo(a, &b, &x, &y) << endl;
+  cout << "b = " << b << endl;
+  cout << "X: {" << x.x << ", " << x.y << ", " << x.z << ", " << x.w << "}" << endl;
+  cout << "Y: {" << y.x << ", " << y.y << ", " << y.z << ", " << y.w << "}" << endl;
+
+  fptr = engine->getPointerToFunction(functions.get("test_ints").func);
+  int (*test_ints)(int, int) = (int (*)(int, int))(fptr);
+  int i = 1;
+  int m = 8;
+  cout << "test_ints(i = " << i << ", m = " << m << ") = " << test_ints(i, m) << endl;
+
+  fptr = engine->getPointerToFunction(functions.get("test_sin").func);
+  float (*test_sin)(float) = (float (*)(float))(fptr);
+
+  a = 1.2;
+  cout << "test_sin(a = " << a << ") = " << test_sin(a) << endl;
+
+  delete engine;
   return 0;
 }
